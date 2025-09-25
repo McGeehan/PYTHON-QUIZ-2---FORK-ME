6a5a2d25d7a01d6e6584d3aba8892345d71d4aca
MkGee <matthewrmcgeehan2@gmail.com>
2025-09-25 09:31:49 -0400

    Add files via upload

---
+++ python-quiz-2.py
@@ -0,0 +1,22 @@
+#Python Quiz 2
+#Fork this repository to your own GitHub account and complete the following tasks in the pythonquiz2.py file.
+
+#Submit a URL link to your completed repository ON GITHUB on Moodle. 
+
+#variables: show an example of variable assignment and usage. Be sure to print the variable to demonstrate its value.
+
+#data types: demonstrate at least three different data types (e.g., integer, string, list) and print their types using the type() function.
+
+
+#functions: define a simple function that takes an argument and returns a value. Call the function and print the result.
+
+
+
+#classes: create a simple class with an __init__ method and one other method. Instantiate the class and call the method, printing the result.
+
+
+
+
+
+
+
